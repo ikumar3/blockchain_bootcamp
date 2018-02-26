0951764ec545efc19500f36dc0de2a18d1a78dec
Kunal  Pareek <kunalpareek@Kunals-MacBook-Air.local>
2018-02-26 16:56:30 +0530

    dapp and readme updated

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
   #### By far the easiest way to install go-ethereum is to use Homebrew tap. If you don't have Homebrew, [install it first](https://brew.sh/).
   1. `brew tap ethereum/ethereum`
   2. `brew install ethereum`
+
+## Installation instruction for Windows
+  1. Download installer from [here](https://gethstore.blob.core.windows.net/builds/geth-windows-amd64-1.8.1-1e67410e.exe)
+  2. Run geth-windows-amd64-1.8.1-1e67410e.exe  
--- dapp/index.js
+++ dapp/index.js
@@ -1,26 +1,134 @@
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
-VotingContract = web3.eth.contract(abi);
+let abi = JSON.parse('[\n' +
+    '\t{\n' +
+    '\t\t"constant": true,\n' +
+    '\t\t"inputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "",\n' +
+    '\t\t\t\t"type": "bytes32"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"name": "votesReceived",\n' +
+    '\t\t"outputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "",\n' +
+    '\t\t\t\t"type": "uint8"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"payable": false,\n' +
+    '\t\t"stateMutability": "view",\n' +
+    '\t\t"type": "function"\n' +
+    '\t},\n' +
+    '\t{\n' +
+    '\t\t"constant": true,\n' +
+    '\t\t"inputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "candidate",\n' +
+    '\t\t\t\t"type": "bytes32"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"name": "validCandidate",\n' +
+    '\t\t"outputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "",\n' +
+    '\t\t\t\t"type": "bool"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"payable": false,\n' +
+    '\t\t"stateMutability": "view",\n' +
+    '\t\t"type": "function"\n' +
+    '\t},\n' +
+    '\t{\n' +
+    '\t\t"constant": true,\n' +
+    '\t\t"inputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "candidate",\n' +
+    '\t\t\t\t"type": "bytes32"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"name": "totalVotesFor",\n' +
+    '\t\t"outputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "",\n' +
+    '\t\t\t\t"type": "uint8"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"payable": false,\n' +
+    '\t\t"stateMutability": "view",\n' +
+    '\t\t"type": "function"\n' +
+    '\t},\n' +
+    '\t{\n' +
+    '\t\t"constant": true,\n' +
+    '\t\t"inputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "",\n' +
+    '\t\t\t\t"type": "uint256"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"name": "candidateList",\n' +
+    '\t\t"outputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "",\n' +
+    '\t\t\t\t"type": "bytes32"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"payable": false,\n' +
+    '\t\t"stateMutability": "view",\n' +
+    '\t\t"type": "function"\n' +
+    '\t},\n' +
+    '\t{\n' +
+    '\t\t"constant": false,\n' +
+    '\t\t"inputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "candidate",\n' +
+    '\t\t\t\t"type": "bytes32"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"name": "voteForCandidate",\n' +
+    '\t\t"outputs": [],\n' +
+    '\t\t"payable": false,\n' +
+    '\t\t"stateMutability": "nonpayable",\n' +
+    '\t\t"type": "function"\n' +
+    '\t},\n' +
+    '\t{\n' +
+    '\t\t"inputs": [\n' +
+    '\t\t\t{\n' +
+    '\t\t\t\t"name": "candidateNames",\n' +
+    '\t\t\t\t"type": "bytes32[]"\n' +
+    '\t\t\t}\n' +
+    '\t\t],\n' +
+    '\t\t"payable": false,\n' +
+    '\t\t"stateMutability": "nonpayable",\n' +
+    '\t\t"type": "constructor"\n' +
+    '\t}\n' +
+    ']');
+let VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('enter contract address here');
-candidates = {"Alex": "candidate-1", "Marty": "candidate-2", "Gloria": "candidate-3"}
+let contractInstance = VotingContract.at('0xb60f44dbf7c99b4921e8365cb6b529627cd8e324');
+let candidates = {"rohan": "candidate-1", "kunal": "candidate-2", "tauseef": "candidate-3"};
 
 function voteForCandidate(candidate) {
-  candidateName = $("#candidate").val();
-  try {
-    contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[0]}, function() {
-      let div_id = candidates[candidateName];
-      $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
-    });
-  } catch (err) {
-  }
+    candidateName = $("#candidate").val();
+    try {
+        contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[0]}, function() {
+            let div_id = candidates[candidateName];
+            contractInstance.totalVotesFor(candidateName, function (err,res) {
+                // console.log(res);
+                $("#" + div_id).html(res.toString());
+            });
+            // $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
+        });
+    } catch (err) {
+    }
 }
 
 $(document).ready(function() {
-  candidateNames = Object.keys(candidates);
-  for (var i = 0; i < candidateNames.length; i++) {
-    let name = candidateNames[i];
-    let val = contractInstance.totalVotesFor.call(name).toString()
-    $("#" + candidates[name]).html(val);
-  }
+    candidateNames = Object.keys(candidates);
+    for (var i = 0; i < candidateNames.length; i++) {
+        let name = candidateNames[i];
+        // var _res;
+        contractInstance.totalVotesFor.call(name, function (err,res) {
+            // console.log(res.toString());
+            $("#" + candidates[name]).html(res.toString());
+        });
+    }
 });
